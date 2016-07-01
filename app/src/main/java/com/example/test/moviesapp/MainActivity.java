e35c4768d56a97e1e077629d526e540ec00dac64
furyjack <lakshaytaneja26@gmail.com>
2016-07-02 02:28:54 +0530

    check network connection on start

--- app/src/main/java/com/example/test/moviesapp/MainActivity.java
+++ app/src/main/java/com/example/test/moviesapp/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.test.moviesapp;
 
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.app.FragmentManager;
@@ -17,6 +20,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -24,7 +28,12 @@ public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "haala";
 
-
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
 
 void settoolbar_nav()
     {
@@ -75,7 +84,12 @@ void settoolbar_nav()
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+        if(!isNetworkAvailable()) {
+            Toast.makeText(MainActivity.this, "Please Connect To Internet", Toast.LENGTH_SHORT).show();
+            finish();
+        }
         settoolbar_nav();
     }
 
